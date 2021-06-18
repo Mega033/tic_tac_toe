4e381a4990524182ff0c41d696da517d7ca2308f
Mega033 <84509755+Mega033@users.noreply.github.com>
2021-06-18 10:45:58 +0200

    board design

--- ttt.cpp
+++ ttt.cpp
@@ -11,9 +11,32 @@ void greet(){
   cout << msg;
 }
 
-int main(){
+
+void display_state(bool p1_marks[3][3], bool p2_marks[3][3], bool is_player1)
+{
     using namespace std;
-    
+
+    string board[3]= {  "___|___|___",
+                        "___|___|___",
+                        "   |   |   " };
+
+}
+
+
+int main()
+{
+    using namespace std;
+
+    bool p1_marks[3][3] = {false};
+    bool p2_marks[3][3] = {false};
+
+    bool is_player1 = true;
+
+    display_state(p1_marks, p2_marks, is_player1);
+
+
+
+
 
     greet();
 
